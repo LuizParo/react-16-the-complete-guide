df636a7c79d285d59f519ef4c4470f716b9e1562
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-06-27 18:30:59 -0300

    passing functions that change the state of a component to another components as props

--- 01-react-complete-guide/src/App.js
+++ 01-react-complete-guide/src/App.js
@@ -14,10 +14,10 @@ class App extends Component {
         otherState : 'some other value'
     }
 
-    switchNameHandler = () => {
+    switchNameHandler = newName => {
         this.setState({
             persons : [
-                { name : 'Maximilian', age : 28 },
+                { name : newName, age : 28 },
                 { name : 'Manu', age : 29 },
                 { name : 'Stephanie', age : 27 }
             ]
@@ -29,10 +29,10 @@ class App extends Component {
             <div className="App">
                 <h1>Hi I'm a Reat App</h1>
                 <p>This is really working!</p>
-                <button onClick={this.switchNameHandler}>Switch</button>
+                <button onClick={() => this.switchNameHandler('Maximilian')}>Switch</button>
 
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'Max!')}>
                     My Hobbies: Racing
                 </Person>
                 <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
