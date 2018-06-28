5adb3c41ce660d0b4e3ace2c751643cb5cb9404b
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-06-27 21:58:24 -0300

    adding style to the App's button

--- 01-react-complete-guide/src/App.js
+++ 01-react-complete-guide/src/App.js
@@ -35,11 +35,19 @@ class App extends Component {
     }
 
     render() {
+        const style = {
+            backgroundColor : 'white',
+            font : 'inherit',
+            border : '1px solid blue',
+            padding : '8px',
+            cursor : 'pointer'
+        };
+
         return (
             <div className="App">
                 <h1>Hi I'm a Reat App</h1>
                 <p>This is really working!</p>
-                <button onClick={() => this.switchNameHandler('Maximilian')}>Switch</button>
+                <button style={style} onClick={() => this.switchNameHandler('Maximilian')}>Switch</button>
 
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
                 <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
