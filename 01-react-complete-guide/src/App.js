7485f3ba351d79f3d5ff37bf57e3e0e65bcb26be
Luiz Guilherme Paro <lparo@daitangroup.com>
2018-06-29 11:22:42 -0300

    fine tuning the persons list update

--- 01-react-complete-guide/src/App.js
+++ 01-react-complete-guide/src/App.js
@@ -15,14 +15,16 @@ class App extends Component {
         showPersons : false
     }
 
-    nameChangedHandler = event => {
-        this.setState({
-            persons : [
-                { id : 1, name : 'Max', age : 28 },
-                { id : 2, name : event.target.value, age : 29 },
-                { id : 3, name : 'Stephanie', age : 27 }
-            ]
-        });
+    nameChangedHandler = (event, id) => {
+        const personIndex = this.state.persons.findIndex(person => person.id === id);
+
+        const person = {...this.state.persons[personIndex]};
+        person.name = event.target.value;
+
+        const persons = [...this.state.persons];
+        persons[personIndex] = person;
+
+        this.setState({ persons });
     }
 
     deletePersonHandler = personIndex => {
@@ -56,7 +58,8 @@ class App extends Component {
                                 key={person.id}
                                 name={person.name}
                                 age={person.age}
-                                click={() => this.deletePersonHandler(index)} />
+                                click={() => this.deletePersonHandler(index)}
+                                changed={event => this.nameChangedHandler(event, person.id)} />
                         })
                     }
                 </div>
