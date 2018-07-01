85131faec7ce53937e827fd271b7406ded8e8770
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-01 20:15:46 -0300

    creating an error boundary component that will display the error if an unexpected error occurs (does not work in dev environment, only production)

--- 01-react-complete-guide/src/App.js
+++ 01-react-complete-guide/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 import classes from './App.css';
 
@@ -48,12 +49,15 @@ class App extends Component {
                 <div >
                     {
                         this.state.persons.map((person, index) => {
-                            return <Person
-                                key={person.id}
-                                name={person.name}
-                                age={person.age}
-                                click={() => this.deletePersonHandler(index)}
-                                changed={event => this.nameChangedHandler(event, person.id)} />
+                            return (
+                                <ErrorBoundary key={person.id}>
+                                    <Person
+                                        name={person.name}
+                                        age={person.age}
+                                        click={() => this.deletePersonHandler(index)}
+                                        changed={event => this.nameChangedHandler(event, person.id)} />
+                                </ErrorBoundary>
+                            );
                         })
                     }
                 </div>
