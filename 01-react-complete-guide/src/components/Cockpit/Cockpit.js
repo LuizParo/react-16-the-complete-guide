87cd928b121d78cfcb7eea69577e4d3110932a6c
Luiz Guilherme Paro <lparo@daitangroup.com>
2018-07-02 09:27:57 -0300

    accessing the props inside a stateful component

--- 01-react-complete-guide/src/components/Cockpit/Cockpit.js
+++ 01-react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -20,7 +20,7 @@ export default props => {
 
     return (
         <div className={classes.Cockpit}>
-            <h1>Hi I'm a Reat App</h1>
+            <h1>{props.appTitle}</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>
             <button className={buttonClass} onClick={props.clicked}>Toggle Persons</button>
         </div>
