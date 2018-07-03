2acef6d60a379ad25d7fdaa41adcfcf94263b0a5
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-03 20:26:36 -0300

    checking the component updating lifecycle hooks triggered by state changes

--- 01-react-complete-guide/src/containers/App.js
+++ 01-react-complete-guide/src/containers/App.js
@@ -12,12 +12,31 @@ class App extends Component {
         console.log('[App.js] Inside constructor', props);
     }
 
+    // ======= COMPONENT CREATION LIFECYCLE HOOKS =======
     componentWillMount() {
         console.log('[App.js] Inside componentWillMount()');
     }
 
     componentDidMount() {
         console.log('[App.js] Inside componentDidMount()');
+    }
+
+    // ======= COMPONENT UPDATING LIFECYCLE HOOKS =======
+    componentWillReceiveProps(nextProps) {
+        console.log('[UPDATE App.js] Inside componentWillReceiveProps()', nextProps);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[UPDATE App.js] Inside shouldComponentUpdate()', nextProps, nextState);
+        return true;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('[UPDATE App.js] Inside componentWillUpdate()', nextProps, nextState);
+    }
+
+    componentDidUpdate() {
+        console.log('[UPDATE App.js] Inside componentDidUpdate()');
     }
 
     state = {
