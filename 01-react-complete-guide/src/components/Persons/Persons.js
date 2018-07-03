f596a4696603dda1801d83eb4c2e9376fa5d5d01
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-03 20:17:55 -0300

    checking the component updating lifecycle hooks

--- 01-react-complete-guide/src/components/Persons/Persons.js
+++ 01-react-complete-guide/src/components/Persons/Persons.js
@@ -9,12 +9,31 @@ export default class Persons extends Component {
         console.log('[Persons.js] Inside constructor', props);
     }
 
+    // ======= COMPONENT CREATION LIFECYCLE HOOKS =======
     componentWillMount() {
         console.log('[Persons.js] Inside componentWillMount()');
     }
 
     componentDidMount() {
         console.log('[Persons.js] Inside componentDidMount()');
+    }
+
+    // ======= COMPONENT UPDATING LIFECYCLE HOOKS =======
+    componentWillReceiveProps(nextProps) {
+        console.log('[UPDATE Persons.js] Inside componentWillReceiveProps()', nextProps);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[UPDATE Persons.js] Inside shouldComponentUpdate()', nextProps, nextState);
+        return nextProps.persons !== this.props.persons;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('[UPDATE Persons.js] Inside componentWillUpdate()', nextProps, nextState);
+    }
+
+    componentDidUpdate() {
+        console.log('[UPDATE Persons.js] Inside componentDidUpdate()');
     }
 
     render() {
