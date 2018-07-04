df5a4738c48cf3e537eb5d471fa4902c66c86ae6
Luiz Guilherme Paro <lparo@daitangroup.com>
2018-07-04 09:02:03 -0300

    passing the props to the component when wraping it with a high order component

--- 01-react-complete-guide/src/hoc/withClass.js
+++ 01-react-complete-guide/src/hoc/withClass.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+// Stateless version
+// const withClass = (WrappedComponent, className) => {
+//     return props => (
+//         <div className={className}>
+//             <WrappedComponent {...props} />
+//         </div>
+//     );
+// };
+
+// Stateful version
 const withClass = (WrappedComponent, className) => {
-    return props => (
-        <div className={className}>
-            <WrappedComponent />
-        </div>
-    );
+    return class extends Component {
+        render() {
+            return (
+                <div className={className}>
+                    <WrappedComponent {...this.props} />
+                </div>
+            );
+        }
+    };
 };
 
 export default withClass;
