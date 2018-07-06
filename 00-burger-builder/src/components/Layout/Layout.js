58ac59bd02582b6896a88ac2fdb52dec28ff697f
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-06 18:25:11 -0300

    creating the Layout component in the burger builder project

---
+++ 00-burger-builder/src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const Layout = props => (
+    <Fragment>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+
+        <main>
+            {props.children}
+        </main>
+    </Fragment>
+);
+
+export default Layout;
