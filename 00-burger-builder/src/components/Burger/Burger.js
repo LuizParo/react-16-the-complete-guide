600556fd1229e099ce0ee1576a50fc90cef7a8fb
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-06 21:38:19 -0300

    dynamically outputting the burger ingredients

--- 00-burger-builder/src/components/Burger/Burger.js
+++ 00-burger-builder/src/components/Burger/Burger.js
@@ -1,22 +1,20 @@
 import React from 'react';
 
-import BurgerIngredient, {
-    BREAD_BOTTOM,
-    BREAD_TOP,
-    MEAT,
-    CHEESE,
-    SALAD,
-    BACON
-} from './BurgerIngredient/BurgerIngredient';
+import BurgerIngredient, { BREAD_BOTTOM, BREAD_TOP } from './BurgerIngredient/BurgerIngredient';
 
 import classes from './Burger.css';
 
 const Burger = props => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(ingredient => {
+            return [...Array(props.ingredients[ingredient])]
+                .map((_, index) => <BurgerIngredient key={ingredient + index} type={ingredient} />);
+        });
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type={BREAD_TOP} />
-            <BurgerIngredient type={CHEESE} />
-            <BurgerIngredient type={MEAT} />
+            {transformedIngredients}
             <BurgerIngredient type={BREAD_BOTTOM} />
         </div>
     );
