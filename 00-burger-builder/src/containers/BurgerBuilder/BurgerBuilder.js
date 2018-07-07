c81b4edc2c4dc4e262b05567017153d1587189be
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-07 13:57:50 -0300

    adding ingredients to the burger clicking in the butttons

--- 00-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ 00-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import React, { Component, Fragment } from 'react';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Burger from '../../components/Burger/Burger';
 
+const INGREDIENT_PRICES = {
+    salad : 0.5,
+    cheese : 0.4,
+    meat : 1.3,
+    bacon : 0.7
+};
+
 class BurgerBuilder extends Component {
 
     state = {
@@ -11,14 +18,33 @@ class BurgerBuilder extends Component {
             bacon : 0,
             cheese : 0,
             meat : 0
-        }
+        },
+        totalPrice : 4
+    }
+
+    addIngredientHandler = type => {
+        const updatedCount = this.state.ingredients[type] + 1;
+        const updatedIngredients = {...this.state.ingredients};
+        const priceAddition = INGREDIENT_PRICES[type];
+        const newPrice = this.state.totalPrice + priceAddition;
+
+        updatedIngredients[type] = updatedCount;
+
+        this.setState({
+            ingredients : updatedIngredients,
+            totalPrice : newPrice
+        });
+    }
+
+    removeIngredientHandler = type => {
+
     }
 
     render() {
         return (
             <Fragment>
                 <Burger ingredients={this.state.ingredients} />
-                <BuildControls />
+                <BuildControls ingredientAdded={this.addIngredientHandler} />
             </Fragment>
         );
     }
