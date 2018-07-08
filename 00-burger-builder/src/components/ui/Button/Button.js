d647c084c399ac7abbc7676018c634506fed36e5
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-08 11:56:38 -0300

    fixing the style for custom Button component

--- 00-burger-builder/src/components/ui/Button/Button.js
+++ 00-burger-builder/src/components/ui/Button/Button.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import classes from './Button';
+import classes from './Button.css';
 
 const Button = props => (
     <button className={[classes.Button, classes[props.buttonType]].join(' ')} onClick={props.clicked}>
