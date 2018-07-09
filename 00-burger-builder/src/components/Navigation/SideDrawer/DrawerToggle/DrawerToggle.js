4b0e9a885b6e5a9fac7f5c7436511e25cd1726e5
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-09 16:38:22 -0300

    adding the burger icon to menu

--- 00-burger-builder/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ 00-burger-builder/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -3,8 +3,10 @@ import React from 'react';
 import classes from './DrawerToggle.css';
 
 const DrawerToggle = props => (
-    <div onClick={props.clicked}>
-        MENU
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
     </div>
 );
 
