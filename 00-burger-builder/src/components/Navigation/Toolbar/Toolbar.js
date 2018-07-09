bea3eeaf80c1501974500674fa0a9d3e991f548c
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-09 11:11:14 -0300

    making the Logo responsive

--- 00-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ 00-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -8,7 +8,11 @@ import classes from './Toolbar.css';
 const Toolbar = props => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <Logo />
+
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+
         <nav>
             <NavigationItems />
         </nav>
