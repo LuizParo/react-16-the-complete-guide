5b0dc5aedc1eb680f0b0495bdc0033b69371ed8d
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-10 22:17:07 -0300

    transforming the posts data before using them

--- 04-blog-application-with-http-requests/src/containers/Blog/Blog.js
+++ 04-blog-application-with-http-requests/src/containers/Blog/Blog.js
@@ -13,12 +13,17 @@ class Blog extends Component {
 
     componentDidMount() {
         axios.get('http://jsonplaceholder.typicode.com/posts')
-            .then(response => this.setState({ posts : response.data }));
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => ({ ...post, author : 'Max' }));
+
+                this.setState({ posts : updatedPosts })
+            });
     }
 
     render () {
         const posts = this.state.posts
-            .map(post => <Post key={post.id} title={post.title}  />);
+            .map(post => <Post key={post.id} title={post.title} author={post.author} />);
 
         return (
             <div>
