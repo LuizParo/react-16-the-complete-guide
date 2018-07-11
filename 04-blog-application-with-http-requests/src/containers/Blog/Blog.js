a165e0ec4f065a4f6d562b8a1f618cb553822051
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-10 22:31:44 -0300

    making a post selectable

--- 04-blog-application-with-http-requests/src/containers/Blog/Blog.js
+++ 04-blog-application-with-http-requests/src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts : []
+        posts : [],
+        selectedPostId : null
     }
 
     componentDidMount() {
@@ -21,9 +22,20 @@ class Blog extends Component {
             });
     }
 
+    postSelectedHandler = id => {
+        this.setState({ selectedPostId : id });
+    }
+
+    _createPost = post => {
+        return <Post key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)} />;
+    }
+
     render () {
         const posts = this.state.posts
-            .map(post => <Post key={post.id} title={post.title} author={post.author} />);
+            .map(this._createPost);
 
         return (
             <div>
@@ -31,7 +43,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
