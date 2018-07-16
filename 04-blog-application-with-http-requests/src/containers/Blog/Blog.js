fd9fc5f44cb5f6fc871e440c55613e259b203d68
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-16 13:21:56 -0300

    placing the root Route definition outside the Switch component

--- 04-blog-application-with-http-requests/src/containers/Blog/Blog.js
+++ 04-blog-application-with-http-requests/src/containers/Blog/Blog.js
@@ -39,8 +39,8 @@ class Blog extends Component {
                     </nav>
                 </header>
 
+                <Route path="/" exact component={Posts} />
                 <Switch>
-                    <Route path="/" exact component={Posts} />
                     <Route path="/new-post" component={NewPost} />
                     <Route path="/:id" exact component={FullPost} />
                 </Switch>
