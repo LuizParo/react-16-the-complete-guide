b3c78c669e1426811be9d53b18c9298fa577c791
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-16 14:02:33 -0300

    conditional redirect using Redirect component

--- 04-blog-application-with-http-requests/src/containers/Blog/NewPost/NewPost.js
+++ 04-blog-application-with-http-requests/src/containers/Blog/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 
 import './NewPost.css';
@@ -7,7 +8,8 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
+        submitted : false
     }
 
     componentDidMount() {
@@ -24,22 +26,33 @@ class NewPost extends Component {
         };
 
         axios.post('/posts', post)
-            .then(console.log);
+            .then(_ => this.setState({ submitted : true }));
+    }
+
+    _renderRedirect() {
+        return this.state.submitted ? <Redirect to="/posts" /> : null;
     }
 
     render () {
+        let redirect = null;
         return (
             <div className="NewPost">
+                {this._renderRedirect()}
+
                 <h1>Add a Post</h1>
+
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+
                 <label>Content</label>
                 <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
+
                 <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
