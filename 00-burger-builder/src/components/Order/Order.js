5c4d31373e1329590f4e8ec9b19046126a290036
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-19 12:12:47 -0300

    dynamically outputting the ingredients inside Order component

--- 00-burger-builder/src/components/Order/Order.js
+++ 00-burger-builder/src/components/Order/Order.js
@@ -2,11 +2,40 @@ import React from 'react';
 
 import classes from './Order.css';
 
-const Order = props => (
-    <div className={classes.Order}>
-        <p>Ingredients: Salad (1)</p>
-        <p>Price: <strong>USD 5.45</strong></p>
-    </div>
-);
+function extractIngredientsArray(ingredientsAsObject) {
+    const ingredients  = [];
+
+    for (let ingredient in ingredientsAsObject) {
+        ingredients.push({
+            name : ingredient,
+            amount : ingredientsAsObject[ingredient]
+        });
+    }
+
+    return ingredients;
+}
+
+const Order = props => {
+    const ingredients = extractIngredientsArray(props.ingredients);
+
+    const ingredientsOutput = ingredients.map(ingredient => {
+        return <span key={ingredient.name} style={{
+                textTransform : 'capitalize',
+                display : 'inline-block',
+                margin : '0 8px',
+                border : '1px solid #ccc',
+                padding : '5px'
+            }}>
+            {ingredient.name}: ({ingredient.amount})
+        </span>;
+    });
+
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientsOutput}</p>
+            <p>Price: <strong>USD {(+props.price).toFixed(2)}</strong></p>
+        </div>
+    );
+}
 
 export default Order;
