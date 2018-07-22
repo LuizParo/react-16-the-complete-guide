18f44f663fee37087d23afa519a911ac1c5fcec8
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-22 14:16:11 -0300

    dispatching the increment action inside the Counter container

--- 06-counter-with-redux/src/store/reducer.js
+++ 06-counter-with-redux/src/store/reducer.js
@@ -3,7 +3,13 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-    return state
+    switch (action.type) {
+        case 'INCREMENT':
+            return { counter : state.counter + 1 };
+
+        default:
+            return state
+    }
 };
 
 export default reducer;
