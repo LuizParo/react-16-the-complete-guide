f424cb8e32192c4a4b62b71965a60b3a1ec22601
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-22 11:59:56 -0300

    dispatching actions in redux

--- 06-counter-with-redux/redux-basics.js
+++ 06-counter-with-redux/redux-basics.js
@@ -5,6 +5,13 @@ const initialState = { counter : 0 };
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INC_COUNTER':
+            return { ...state, counter : state.counter + 1 };
+
+        case 'ADD_COUNTER':
+            return { ...state, counter : state.counter + action.value };
+    }
     return state;
 };
 
@@ -13,5 +20,8 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching Action
+store.dispatch({ type : 'INC_COUNTER' });
+store.dispatch({ type : 'ADD_COUNTER', value : 10 });
+console.log(store.getState());
 
 // Subscription
