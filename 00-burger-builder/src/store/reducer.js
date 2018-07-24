2cdfebb16ac4d0edf770c90d079c8a3269e83d97
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-24 13:50:22 -0300

    installing redux and react-redux packages in the BurgerBuilder project

---
+++ 00-burger-builder/src/store/reducer.js
@@ -0,0 +1,12 @@
+import { ADD_INGREDIENT, REMOVE_INGREDIENT } from './actions';
+
+const initialState = {
+    ingredients : [],
+    totalPrice : 4
+};
+
+const reducer = (state = initialState, action) => {
+    
+};
+
+export default reducer;
