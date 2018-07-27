6f6ec7d06c3772668ba66a2c12c6158364ef2fcf
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-26 22:12:39 -0300

    exporting the switch cases logic to functions

--- 06-counter-with-redux/src/store/reducers/result.js
+++ 06-counter-with-redux/src/store/reducers/result.js
@@ -5,17 +5,23 @@ const initialState = {
     results : []
 };
 
+const storeResult = (state, action) => {
+    const newArray = state.results.concat({ id : new Date(), value : action.result });
+    return updateObject(state, { results : newArray });
+};
+
+const deleteResult = (state, action) => {
+    const newArray = state.results.filter(result => result.id !== action.resultId);
+    return updateObject(state, { results : newArray });
+};
+
 const resultReducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.STORE_RESULT: {
-            const newArray = state.results.concat({ id : new Date(), value : action.result });
-            return updateObject(state, { results : newArray });
-        }
+        case actionTypes.STORE_RESULT:
+            return storeResult(state, action);
 
-        case actionTypes.DELETE_RESULT: {
-            const newArray = state.results.filter(result => result.id !== action.resultId);
-            return updateObject(state, { results : newArray });
-        }
+        case actionTypes.DELETE_RESULT:
+            return deleteResult(state, action);
 
         default:
             return state
