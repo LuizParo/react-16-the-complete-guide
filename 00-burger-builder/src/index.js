c8235ccd19fda3a4db96d3f9bdaeb85e75c21bd8
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-28 12:04:05 -0300

    adding redux-thunk to BurgerBuilder project

--- 00-burger-builder/src/index.js
+++ 00-burger-builder/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './App';
 
@@ -12,9 +13,11 @@ import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+    composeEnhancers(applyMiddleware(reduxThunk))
 );
 
 ReactDOM.render(
