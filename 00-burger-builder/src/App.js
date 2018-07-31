c6f27efab68db61d27e1dbff8e67ca02c2019310
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-07-31 15:28:03 -0300

    creating the Logout container to logout users

--- 00-burger-builder/src/App.js
+++ 00-burger-builder/src/App.js
@@ -6,6 +6,7 @@ import Layout from './hoc/Layout/Layout';
 import Auth from './containers/Auth/Auth';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Logout from './containers/Auth/Logout/Logout';
 import Orders from './containers/Orders/Orders';
 
 class App extends Component {
@@ -16,6 +17,7 @@ class App extends Component {
                 <Layout>
                     <Switch>
                         <Route path="/auth" component={Auth} />
+                        <Route path="/logout" component={Logout} />
                         <Route path="/checkout" component={Checkout} />
                         <Route path="/orders" component={Orders} />
                         <Route path="/" component={BurgerBuilder} />
