debe0affb9333a2e6d338d764d9ccd419611422e
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-08-14 20:06:52 -0500

    adding babel-polyfill to the application

--- 08-webpack-raw-project/src/index.js
+++ 08-webpack-raw-project/src/index.js
@@ -1,3 +1,5 @@
+import 'babel-polyfill';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
@@ -5,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 import './index.css';
+
 
 ReactDOM.render(
     <BrowserRouter>
