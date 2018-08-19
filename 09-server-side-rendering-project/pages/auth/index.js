4422d95dac6f3f2b27ca40a26908ff4a99072b95
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-08-19 16:58:48 -0500

    exploring the getInitialProps lifecycle method added by nextjs

--- 09-server-side-rendering-project/pages/auth/index.js
+++ 09-server-side-rendering-project/pages/auth/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 import User from '../../components/User';
 
-const Index = () => (
+const Index = props => (
     <div>
-        <h1>The Auth Index Page</h1>
+        <h1>The Auth Index Page - {props.appName}</h1>
         <User name="Max" age={28} />
     </div>
 );
+
+Index.getInitialProps = context => {
+    console.log('[pages/auth/Index.js] getInitialProps', context);
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ appName : 'Super App (Auth)' });
+        }, 1000);
+    });
+};
 
 export default Index;
--- 09-server-side-rendering-project/pages/index.js
+++ 09-server-side-rendering-project/pages/index.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 
-const Index = () => (
-    <div>
-        <h1>The Main Page</h1>
-        <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
-        <button onClick={() => Router.push('/auth')}>Go to Auth</button>
-    </div>
-);
+class Index extends Component {
+
+    // Lifecycle hook from nextjs. Excuted on the server alone (not displayed in the browser).
+    static getInitialProps(context) {
+        console.log('[pages/Index.js] getInitialProps', context);
+
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({ appName : 'Super App' });
+            }, 1000);
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>The Main Page of {this.props.appName}</h1>
+                <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
+                <button onClick={() => Router.push('/auth')}>Go to Auth</button>
+            </div>
+        );
+    }
+}
 
 export default Index;
