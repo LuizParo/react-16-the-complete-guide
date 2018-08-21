1ac5a1b5f59dd128c3bdd3afd347027d7aa7b18e
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-08-21 18:45:27 -0500

    putting the animation logic inside the Modal component

--- 10-animations-project/src/components/Modal/Modal.js
+++ 10-animations-project/src/components/Modal/Modal.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import Transition from 'react-transition-group/Transition';
 
 import './Modal.css';
 
-const modal = (props) => {
-    const modalClosed = props.show ==='exiting' ? 'ModalClosed' : null;
-    const classes = ['Modal', props.show ==='entering' ? 'ModalOpen' : modalClosed];
+const modal = (props) => (
+    <Transition in={props.show} timeout={300} mountOnEnter unmountOnExit>
+        {state => {
+            const modalClosed = state === 'exiting' ? 'ModalClosed' : null;
+            const classes = ['Modal', state === 'entering' ? 'ModalOpen' : modalClosed];
 
-    return (
-        <div className={classes.join(' ')}>
-            <h1>A Modal</h1>
-            <button className="Button" onClick={props.closed}>Dismiss</button>
-        </div>
-    );
-};
+            return (
+                <div className={classes.join(' ')}>
+                    <h1>A Modal</h1>
+                    <button className="Button" onClick={props.closed}>Dismiss</button>
+                </div>
+            );
+        }}
+    </Transition>
+);
 
 export default modal;
